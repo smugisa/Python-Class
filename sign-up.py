9d64a1f7fdcbfe34c79d319f46a9288adca4e289
Stephen Mugisa <smugisa3@gmail.com>
2018-08-06 16:11:38 +0300

    add validate_email

--- sign-up.py
+++ sign-up.py
@@ -1,17 +1,25 @@
-from database import Database
+import re
 
 class SignUP(object):
     def __init__(self, first_name, last_name, email):
         self.first_name = first_name
         self.last_name = last_name
-        self.email = email
+        self.email = SignUP.validate_email(email)
 
     def full_name(self):
         return '{} {}'.format(self.first_name, self.last_name)
     
     def submit(self):
         pass
-        
-one = SignUP('Amos', 'Mike', 'amos@gmail.com')
 
-SignUP.submit(one)
+    @staticmethod
+    def validate_email(email):
+        match = re.match('^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,4})$', email)
+
+        if match == None:
+            return "invalid email address"
+        
+        else:
+            print("email is valid")
+            return email
+            
